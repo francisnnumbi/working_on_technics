3a8ba56452443851e65f0f4923d710bbc75f26e9
Francis Nduba Numbi <francisnnumbi@yandex.com>
2022-07-03 12:25:54 +0200

    this

--- lib/modules/module13/models/address/address.dart
+++ lib/modules/module13/models/address/address.dart
@@ -1,4 +1,8 @@
-class Address {
+import 'package:hive_flutter/hive_flutter.dart';
+
+@HiveType(typeId: 0)
+class Address extends HiveObject {
+  @HiveField(0)
   String? address;
 
   Address({this.address});
--- lib/modules/module13/models/person/person.dart
+++ lib/modules/module13/models/person/person.dart
@@ -1,7 +1,11 @@
+import 'package:hive_flutter/adapters.dart';
 import 'package:working_on_technics/modules/module13/models/address/address.dart';
 
+@HiveType(typeId: 1)
 class Person {
+  @HiveField(0)
   late String name;
+  @HiveField(1)
   late List<Address> addresses;
 
   Person({required this.name, required this.addresses});
