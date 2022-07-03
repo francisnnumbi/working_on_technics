a17bc4f97dba3aba01284b5b794b0ac7f76f1dd1
Francis Nduba Numbi <francisnnumbi@yandex.com>
2022-07-03 11:20:51 +0200

    f

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:working_on_technics/modules/module12/auth_screen.dart';
+import 'package:working_on_technics/modules/module13/drifting.dart';
 
 import 'modules/module1/pull_to_refresh.dart';
 import 'modules/module11/pdf_controller.dart';
@@ -27,54 +28,7 @@ class MyApp extends StatelessWidget {
       // home: const TestScreen(),
       //home: Module3Screen(),
       // home: BaseWidget(data: "Crazy", child: AnyWidget()),
-      home: AuthScreen(),
-    );
-  }
-}
-
-class TestScreen extends StatefulWidget {
-  const TestScreen({Key? key}) : super(key: key);
-
-  @override
-  State<TestScreen> createState() => _TestScreenState();
-}
-
-class _TestScreenState extends State<TestScreen> {
-  List<String> ll = <String>["First item", "Second item", "Third item"];
-  final PullToRefreshController _controller = PullToRefreshController();
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _controller.addListener(() async {
-      if (_controller.endPulling) {
-        // simulate a long running task
-        await Future.delayed(const Duration(seconds: 3));
-        setState(() {
-          ll.add(DateTime.now().toString());
-        });
-        _controller.setLoaded();
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: PullToRefresh(
-        // maxHeight: 300,
-        controller: _controller,
-        child: ListView.builder(
-            shrinkWrap: true,
-            //   physics: const NeverScrollableScrollPhysics(),
-            itemCount: ll.length,
-            itemBuilder: (_, idx) {
-              return ListTile(
-                title: Text(ll[idx]),
-              );
-            }),
-      ),
+      home: Drifting(),
     );
   }
 }
---
+++ lib/modules/module13/drifting.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Drifting extends StatelessWidget {
+  const Drifting({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Drifting')),
+    );
+  }
+}
